83a7221117fe87d0e36b34877a86bb61e0cca039
LayerDE <the_gamer@gmx.net>
2017-10-25 21:27:25 +0200

    windows support in code...
    todo:
    windows compiler file

--- Global/API_header.h
+++ Global/API_header.h
@@ -1,5 +1,9 @@
 #ifdef __WIN32
 
+#define LoadLib(path) LoadLibrary(path)
+#define LoadFunc(lib_handle,function) GetProcAddress(lib_handle, function)
+#define UnloadLib(lib_handle) FreeLibrary(lib_handle)
+
 #define API_HIDE 
 #define API_SHOW __attribute__ ((dllexport))
 
@@ -7,6 +11,10 @@
 #define API_SHOW_var __attribute__ ((dllexport))
 
 #else
+
+#define LoadLib(path) dlopen(path,RTLD_LAZY)
+#define LoadFunc(lib_handle,function) dlsym(lib_handle, function)
+#define UnloadLib(lib_handle) FreeLibrary(lib_handle)
 
 #define API_HIDE __attribute__ ((visibility ("hidden")))
 #define API_SHOW __attribute__ ((visibility ("default")))
--- Launcher/launcher.c
+++ Launcher/launcher.c
@@ -27,9 +27,9 @@ uint8_t Char2Int(char hex)
 int main(){
 	uint8_t eeprom_test[64*1024];
 	uint8_t* test_eeprom=eeprom_test;
-	void* file_reader = dlopen("./hex_reader.so",RTLD_LAZY);
+	void* file_reader = LoadLib("./hex_reader.so");
 	printf("debug\n");
-	if(((loadHEX_func)dlsym(file_reader,"read_IntelHEX"))(test_eeprom,"../tests/test2.hex"))
+	if(((loadHEX_func)LoadFunc(file_reader,"read_IntelHEX"))(test_eeprom,"../tests/test2.hex"))
 		printf("loading complete\n");
 	else
 		printf("loading failed");
